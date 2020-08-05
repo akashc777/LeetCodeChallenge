41f900dddb411f5ba076bcea2fb7e896bb16c966
akashc777 <contact@akash.page>
2020-08-05 22:10:03 +0530

    5th Aug 2020

--- testrun/main.cpp
+++ testrun/main.cpp
@@ -1,26 +1,41 @@
-#include<iostream>
-#include<vector>
-#include<string>
-#include<algorithm>
-
+#include<bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-
-bool comp(string s1, string s2){
-        int  a =0, b=0;
-        for(int i: s1){
-            a+=i;
-        }
-        for(int i: s1){
-            b+=i;
-        }
-        return a==b;
-    }
-
-int main(){
-    vector<string> strs({ "eat", "tea", "tan", "ate", "nat", "bat"});
-    sort(strs.begin(), strs.end(), comp);
-    for(string i: strs){
-        cout<<i<<endl;
-    }
+ 
+#define ff              first
+#define ss              second
+#define int             long long
+#define pb              push_back
+#define mp              make_pair
+#define pii             pair<int,int>
+#define vi              vector<int>
+#define mii             map<int,int>
+#define pqb             priority_queue<int>
+#define pqs             priority_queue<int,vi,greater<int> >
+#define setbits(x)      __builtin_popcountll(x)
+#define zrobits(x)      __builtin_ctzll(x)
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n,type)  type *arr=new type[n];
+#define w(x)            int x; cin>>x; while(x--)
+mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
+ 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
+ 
+void c_p_c()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+ 
+int32_t main()
+{
+    c_p_c();
     return 0;
 }
